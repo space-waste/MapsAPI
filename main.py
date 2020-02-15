69291e01eb9cabc75a3f1bd7d21775b89c6c7827
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2020-02-15 18:40:12 +0300

    Добавления класса главного окна
    +Класс главного окна, который может принимать координаты и масштаб и рисовать картинку с картой на форме.

--- main.py
+++ main.py
@@ -1,2 +1,61 @@
 #главный рабочий класс проекта
 #при изменениях в реквестах очень прошу писать комментари к коммиту
+
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import uic
+
+
+class MyMap(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.x, self.y, self.masht = '37.530887', '55.703118', '0.002'
+
+        uic.loadUi('1.ui', self)
+        self.pushButton.clicked.connect(self.setImageToPixmap)
+
+        # 37.530887, 55.703118
+        self.map_request = ['http://static-maps.yandex.ru/1.x/?ll=', self.x, ',', self.y,
+                            '&spn=', self.masht, ',', self.masht, '&l=map']
+
+    def getImage(self):
+        self.x = self.edit_x.toPlainText()
+        self.y = self.edit_y.toPlainText()
+        self.masht = self.mashtab.toPlainText()
+        self.map_request = ''.join(['http://static-maps.yandex.ru/1.x/?ll=', self.x, ',',
+                                    self.y, '&spn=', self.masht, ',', self.masht, '&l=map'])
+        response = requests.get(self.map_request)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(self.map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            return "Ошибка выполнения запроса:" + '\n' + "Http статус:", response.status_code, "(", response.reason, ")"
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+        return 'успех'
+
+    def setImageToPixmap(self):
+        is_all_secc = self.getImage()
+        if is_all_secc == 'успех':
+            self.pixmap = QPixmap(self.map_file)
+            self.image.setPixmap(self.pixmap)
+        else:
+            self.image.setText(is_all_secc)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyMap()
+    ex.show()
+    sys.exit(app.exec())
+
